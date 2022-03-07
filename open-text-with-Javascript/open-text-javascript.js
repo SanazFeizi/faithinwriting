fb570c27127048c9ee40a0f27ed1c2bee47955ee
SanazFeizi <sanazfeizikhankandi@gmail.com>
2022-03-07 17:56:58 -0500

    open text- javascript

---
+++ open-text-with-Javascript/open-text-javascript.js
@@ -0,0 +1,55 @@
+/* show specific text according to the time*/
+
+let today = new Date();
+ let hour = today.getHours();
+ let message;
+ let messageContainer = document.getElementById("message");
+
+ if (hour >=18) {
+		message = " Can you buy chewing gum?"
+	} else if (hour >=12) {
+		message = "..."
+	} else {
+		message = "Can you hear me?"
+	}
+
+messageContainer.innerHTML = message;
+
+
+/* show specific text according to the time*/ 
+   
+let day = new Date();
+ let time = today.getHours();
+ let myId;
+ let myIdContainer = document.getElementById("myId");
+
+ if (hour >=18) {
+		myId = " her laughter"
+	} else if (hour >=12) {
+		myId = "her face"
+	} else {
+		myId = "her eyes"
+	}
+
+myIdContainer.innerHTML = myId;
+
+
+/*show/hide the last part of text according to the time*/
+
+let currentTime = new Date()
+ let currentday = today.getHours();
+ let myDIV;
+ let myDIVContainer = document.getElementById("myDIV");
+
+ if(hour >=18)
+	  {
+		  document.getElementById("myDIV").style.display = 'block';
+	  }
+	 else if (hour >=12) {
+		document.getElementById("myDIV").style.display = 'none';
+	}
+	  else {
+		  document.getElementById("myDIV").style.display = 'block';
+ }
+
+
