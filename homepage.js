875f9fab269560e1272a7db1771491a796b5a61d
SanazFeizi <sanazfeizikhankandi@gmail.com>
2022-04-11 21:15:03 -0400

    trying to fix the link problem

---
+++ homepage.js
@@ -0,0 +1,26 @@
+let counter = 0;
+let button = document.querySelector(".myId");
+console.log(counter);
+button.addEventListener("click", addName);
+
+function addName() {
+    counter++;
+    console.log(counter);
+
+    if (counter > 0) {
+        
+        button.innerHTML = "Faith in writing"
+    }
+
+    if (counter > 1) {
+        button.innerHTML = "A website"
+    }
+
+    if (counter > 2) {
+        button.innerHTML = "by: Sanaz Feizi"
+    }
+
+    if (counter > 2) {
+        counter = 0;
+    }  
+}
